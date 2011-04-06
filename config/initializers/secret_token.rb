39cb5b3cfedbd2efab9d208e3a096dd9d00ba7b3
David Kamphausen <davidkamphausen@david-kamphausens-macbook-pro.local>
2011-04-06 16:13:38 +0200

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gezwitscher::Application.config.secret_token = '162c6ba80e08c58586fc254a9d44b6ca905ffc2e5d8271064ec04e51210a6753dc9e17b7ab89beb7233ce5b820be873d3fe5012a0c7c725911480dad59457583'

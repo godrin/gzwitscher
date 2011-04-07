d28930e7b89827ad72b8416f54403c312e376e37
David Kamphausen <davidkamphausen@david-kamphausens-macbook-pro.local>
2011-04-07 16:18:42 +0200

    added bonus and prevent xss

--- app/models/message_observer.rb
+++ app/models/message_observer.rb
@@ -6,5 +6,13 @@ class MessageObserver < ActiveRecord::Observer
       rel.message_id=message.id
       rel.save
     }
+
+    User.find_by_sql(["select users.* from users,messages where text like '%'||email||'%' and messages.id=?",message.id]).each{|user|
+      rel=MessageRelation.new
+      rel.user_id=user.id
+      rel.message_id=message.id
+      rel.save
+    }
+
   end
 end
